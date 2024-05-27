f38e846c2010e0601952c5834e97e9e2c5e1dfb4
dongjilo <121302078+dongjilo@users.noreply.github.com>
2024-05-28 00:27:19 +0800

    updated login & register cards

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -4,7 +4,7 @@
 
 @section('content')
     <div class="row justify-content-center">
-        <div class="col-lg-7">
+        <div class="col-lg-4">
             <div class="card shadow-lg my-5">
                 <div class="card-body p-0">
                     <div class="row">
