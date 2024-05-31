1ed44926080771d2cc71edd2fd6f077e04693037
dongjilo <121302078+dongjilo@users.noreply.github.com>
2024-05-31 19:19:53 +0800

    added error alert

--- resources/views/goals/index.blade.php
+++ resources/views/goals/index.blade.php
@@ -16,6 +16,14 @@
                 </button>
             </div>
         @endif
+        @if(session('error'))
+            <div class="alert alert-danger alert-dismissible fade show" role="alert" id="errorAlert">
+                <ul class="list-unstyled mb-0">
+                    <li>{{ session('error') }}</li>
+                </ul>
+                <button type="button" class="close" aria-label="Close" onclick="dismissAlert('errorAlert')"><span aria-hidden="true">&times;</span></button>
+            </div>
+        @endif
 
         @php
             $settings = Auth::user()->settings->first();
@@ -27,11 +35,10 @@
                 <div class="col-lg-4 mb-4">
                     <div class="card shadow">
                         <div class="card-body">
-                            <h5 class="card-title">Goal: {{ $currencySymbol }} {{ number_format($goal->target_amount, 2) }}</h5>
+                            <h5 class="card-title">Goal #{{$goal->id}}: {{ $currencySymbol }} {{ number_format($goal->target_amount, 2) }}</h5>
                             <p class="card-text">
                                 Current Progress: {{ $currencySymbol }} {{ number_format($goal->current_amount, 2) }}
-
-                            <div class="progress mb-4">
+                            <div class="progress mb-2">
                                 <div class="progress-bar {{ $goal->progress >= 100 ? 'bg-success' : '' }}" role="progressbar"
                                      style="width: {{ number_format($goal->progress, 2) }}%"
                                      aria-valuenow="{{ number_format($goal->progress, 2) }}" aria-valuemin="0" aria-valuemax="100">
@@ -45,7 +52,7 @@
                             <form action="{{ route('goals.destroy', $goal->id) }}" method="POST" class="d-inline">
                                 @csrf
                                 @method('DELETE')
-                                <button type="submit" class="btn btn-danger" onclick="return confirm('Do you really want to delete the Goal: {{$goal->target_amount}}?')">Delete</button>
+                                <button type="submit" class="btn btn-danger" onclick="return confirm('Do you really want to delete Goal #{{$goal->id}}?')">Delete</button>
                             </form>
                         </div>
                     </div>
